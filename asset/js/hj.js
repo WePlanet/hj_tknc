c386b28984139a1df8a9931446a1fdda2c41ee5a
hodoogwaja <hodoogwaja@gmail.com>
2016-12-29 04:28:00 +0900

    a태그 수정,1-1-2,2-5-1

--- asset/js/hj.js
+++ asset/js/hj.js
@@ -6,4 +6,31 @@ $( document ).ready(function() {
     });
   });
 
+  // 모든 A 태그에 data-ajax='false 추가
+  var allATag = document.getElementsByTagName('a');
+  (function(){
+    for(var i=0; i<allATag.length; i++){
+      allATag[i].setAttribute('data-ajax','false');
+    }
+  }());
+
+  // 2-5-1 플러스 마이너스 버튼
+  (function(){
+    var pQ = document.getElementById("productQuantity").value;
+      $("#plusBtn").click(function(){
+        pQ ++;
+        $("#productQuantity").val(pQ);
+      });
+      $("#minusBtn").click(function(){
+        if(pQ > 0){
+          pQ --;
+          $("#productQuantity").val(pQ);
+        }
+        else{
+          alert("0보다 작을 수 없어요.")
+        }
+      });
+
+  }());
+
 });
